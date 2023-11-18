92d34e95360fcdb694d45aedbe5b65c9f8f6dff9
David-EU-PL <dawidplatek.eu.pl@outlook.com>
2023-11-18 21:20:35 +0100

    Add project files.

---
+++ BlazorHomework1/Pages/Home.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorHomework1.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+        public void GoToHomework()
+        {
+            NavigationManager.NavigateTo("/homework/parameter");
+        }
+    }
+}
---
+++ BlazorHomework1/Pages/Homework.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorHomework1.Pages
+{
+    public partial class Homework
+    {
+        [Parameter]
+        public string Header { get; set; }
+
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+        private void GoToHome()
+        {
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
